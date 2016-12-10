881d6558079e493783c4573431754f349c569232
Benjamin Borbe <bborbe@rocketnews.de>
2016-12-10 15:29:06 +0100

    add api server port

--- scripts/admin-kubectl-configure.sh
+++ scripts/admin-kubectl-configure.sh
@@ -8,7 +8,7 @@ set -o errtrace
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})
 
 mkdir -p $HOME/.kube/nc
-kubectl config set-cluster nc-cluster --server=https://185.170.112.48:443 --certificate-authority=$HOME/.kube/nc/ca.pem
+kubectl config set-cluster nc-cluster --server=https://185.170.112.48:6443 --certificate-authority=$HOME/.kube/nc/ca.pem
 kubectl config set-credentials nc-admin --certificate-authority=$HOME/.kube/nc/ca.pem --client-key=$HOME/.kube/nc/admin-key.pem --client-certificate=$HOME/.kube/nc/admin.pem
 kubectl config set-context nc-system --cluster=nc-cluster --user=nc-admin
 kubectl config use-context nc-system
