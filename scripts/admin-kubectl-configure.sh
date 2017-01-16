a46e5b9f2b9f033f63f8942bf1a698a83be3dd6b
Benjamin Borbe <bborbe@rocketnews.de>
2017-01-16 21:00:35 +0100

    update k8s

--- scripts/admin-kubectl-configure.sh
+++ scripts/admin-kubectl-configure.sh
@@ -7,10 +7,10 @@ set -o errtrace
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})
 
-mkdir -p $HOME/.kube/nc
-kubectl config set-cluster nc-cluster --server=https://185.170.112.48:6443 --certificate-authority=$HOME/.kube/nc/ca.pem
-kubectl config set-credentials nc-admin --certificate-authority=$HOME/.kube/nc/ca.pem --client-key=$HOME/.kube/nc/admin-key.pem --client-certificate=$HOME/.kube/nc/admin.pem
-kubectl config set-context nc-system --cluster=nc-cluster --user=nc-admin
-kubectl config use-context nc-system
+mkdir -p $HOME/.kube/netcup
+kubectl config set-cluster netcup-cluster --server=https://185.170.112.48:6443 --certificate-authority=$HOME/.kube/netcup/ca.pem
+kubectl config set-credentials netcup-admin --certificate-authority=$HOME/.kube/netcup/ca.pem --client-key=$HOME/.kube/netcup/admin-key.pem --client-certificate=$HOME/.kube/netcup/admin.pem
+kubectl config set-context netcup-system --cluster=netcup-cluster --user=netcup-admin
+kubectl config use-context netcup-system
 
 echo "test with 'kubectl get nodes'"
